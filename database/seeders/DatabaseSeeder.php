b4b33970e5cde5b024e59da1602f643c836020e5
SuryaPratama-Surz <manjirotama@gmail.com>
2025-07-08 08:21:20 +0700

    auth

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -11,14 +10,21 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+    // public function run(): void
+    // {
+    //     // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => 'tes123',
-        ]);
-    }
+    //     User::factory()->create([
+    //         'name' => 'Test User',
+    //         'email' => 'test@example.com',
+    //         'password' => 'tes123',
+    //     ]);
+    // }
+
+        public function run()
+{
+    // Jalankan seeder AdminUserSeeder
+    $this->call(AdminUserSeeder::class);
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+//Route AKSES BLOKIR
+Route::middleware('is_admin')->group(function () {
+    // Route::resource('pengguna', PenggunasController::class);
+});
